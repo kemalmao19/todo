79f814dab641a0f214c98df36de28ef570255801
kemalmao19 <sebelumtidur14@gmail.com>
2023-11-25 17:41:45 +0700

    add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 export const Footer = () => {
   return (
     <div className='mt-10 flex flex-col items-center gap-3'>
-        <p className='text-cyan-400'>Love to hear any feedback ❤️</p>
+        <p className='text-cyan-400 font-comic'>Love to hear any feedback ❤️</p>
         <div className='flex gap-3'>
             <Link href="https://github.com/kemalmao19"><Image src={"/github.svg"} width={20.0} height={20.0} alt='github'/></Link>
             <Link href="mailto:kemalsmy@gmail.com"><Image src={"/email.svg"} width={20.0} height={20.0} alt='email'/></Link>
